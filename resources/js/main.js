6f2ee8742f2025e08eef8b69c8514708be1383b9
Paulo Astorga <pauloastorga.docs@gmail.com>
2021-12-11 02:54:01 -0300

    Modificacion de paleta de colores y de CSS en general

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,17 +1,44 @@
-/*SlideShow*/
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("carousel__banner");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000);
+/*Menu show and hidden (si, copypasteado porque lo hice yo 😊 )*/
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*Menu show*/
+if (navToggle) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
 }
+
+/*Menu hidden*/
+if (navClose) {
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
 /*-----*/
 
+/*Aside show and hidden*/
+
+const menuAside = document.getElementById('main-aside'),
+        showAside = document.getElementById('show-aside'),
+        hideAside = document.getElementById('hide-aside')
+    
+if (hideAside) {
+    hideAside.addEventListener('click', () =>{
+        menuAside.classList.add('hide__aside')
+        hideAside.classList.add('hide__eye')
+    })
+}
+
+if (showAside) {
+    showAside.addEventListener('click', () =>{
+        menuAside.classList.remove('hide__aside')
+        hideAside.classList.remove('hide__eye')
+    })
+}
+
+/*-----*/
